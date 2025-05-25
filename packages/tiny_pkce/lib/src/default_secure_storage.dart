9333777d608e644753a83b90bf3cd2c1a35d859d
Yannick <yannick.stolle@gmail.com>
2025-05-25 13:45:02 +0200

    feat: add key prefix support to DefaultSecureStorage to enable having several auth services in a single app

--- packages/tiny_pkce/lib/src/default_secure_storage.dart
+++ packages/tiny_pkce/lib/src/default_secure_storage.dart
@@ -4,15 +4,27 @@ import 'package:tiny_pkce/src/secure_storage.dart';
 
 /// Default implementation using FlutterSecureStorage
 class DefaultSecureStorage implements SecureStorage {
+  /// Creates a new [DefaultSecureStorage] with a default key prefix of ''.
+  ///
+  /// The [keyPrefix] is used to prefix all keys stored in the secure storage.
+  /// This is useful to avoid conflicts with other keys stored in the secure
+  /// storage.
+
+  DefaultSecureStorage({String keyPrefix = ''}) : _keyPrefix = keyPrefix;
+
+  final String _keyPrefix;
+
   final _storage = const FlutterSecureStorage();
 
   @override
   Future<void> write({required String key, required String? value}) =>
-      _storage.write(key: key, value: value);
+      _storage.write(key: '$_keyPrefix$key', value: value);
 
   @override
-  Future<String?> read({required String key}) => _storage.read(key: key);
+  Future<String?> read({required String key}) =>
+      _storage.read(key: '$_keyPrefix$key');
 
   @override
-  Future<void> delete({required String key}) => _storage.delete(key: key);
+  Future<void> delete({required String key}) =>
+      _storage.delete(key: '$_keyPrefix$key');
 }
